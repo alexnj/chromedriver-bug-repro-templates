b3cd1cdba24eff5df80c0a9e7329bf2a18503e10
Alex N. Jose <alexnj@google.com>
2026-01-07 13:23:45 -0800

    Move server to same process.

--- selenium-nightwatch/nightwatch.conf.js
+++ selenium-nightwatch/nightwatch.conf.js
@@ -1,5 +1,36 @@
+const http = require('http');
+const fs =require('fs');
+const path = require('path');
+
+let server;
+
 module.exports = {
   src_folders: ['test.js'],
+
+  before: function(done) {
+    server = http.createServer((req, res) => {
+      fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+        if (err) {
+          res.writeHead(500);
+          res.end('Error loading index.html');
+          return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      });
+    }).listen(3000, 'localhost', () => {
+      console.log('Server started on port 3000');
+      done();
+    });
+  },
+
+  after: function(done) {
+    server.close(() => {
+      console.log('Server stopped');
+      done();
+    });
+  },
+
   test_settings: {
     default: {
       launch_url: 'http://localhost:3000',
