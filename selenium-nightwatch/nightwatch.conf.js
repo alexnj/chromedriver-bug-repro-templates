45ad9419b4edc3f45b8f0bd43d61e5a6f427a2a0
Alex N. Jose <alexnj@google.com>
2026-01-07 15:08:54 -0800

    Fix server to global

--- selenium-nightwatch/nightwatch.conf.js
+++ selenium-nightwatch/nightwatch.conf.js
@@ -1,35 +1,6 @@
-const http = require('http');
-const fs =require('fs');
-const path = require('path');
-
-let server;
-
 module.exports = {
   src_folders: ['test.js'],
-
-  before: function(done) {
-    server = http.createServer((req, res) => {
-      fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
-        if (err) {
-          res.writeHead(500);
-          res.end('Error loading index.html');
-          return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      });
-    }).listen(8080, '127.0.0.1', () => {
-      console.log('Server started on port 8080');
-      done();
-    });
-  },
-
-  after: function(done) {
-    server.close(() => {
-      console.log('Server stopped');
-      done();
-    });
-  },
+  globals_path: 'globals.js',
 
   test_settings: {
     default: {
@@ -40,13 +11,15 @@ module.exports = {
         server_path: require('chromedriver').path,
         cli_args: [
           '--silent'
-        ]
+        ],
+        log_path: 'chromedriver.log',
       },
       desiredCapabilities: {
         browserName: 'chrome',
         chromeOptions: {
           args: [
-            '--headless' // Uncomment for headless mode
+            '--headless',
+            '--no-sandbox'
           ]
         }
       }
